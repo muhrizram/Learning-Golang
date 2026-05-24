89c8314fad2e0cc0b4a64ae01ee15560b8ff4f77
M Rizki Ramadhan <p79-muhammad.ramadan@edot-dev.com>
2026-05-24 16:36:21 +0700

    exercise-01

---
+++ exercise-01/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World! 😎")
+	fmt.Println(`
+A Necessary Autumn Inside Each
+~Rumi~
+
+You and I have spoken all these words,
+but as for the way we have to go,
+words are no preparation.
+There is no getting ready, other than grace....
+Inside each of us, there’s continual autumn.
+Our leaves fall and are blown out over the water.
+A crow sits in the blackened limbs
+and talks about what’s gone....
+There’s a necessary dying,
+and then Jesus is breathing again.
+Very little grows on jagged rock.
+Be ground. Be crumbled, so wildflowers
+will come up where you are.
+You’ve been stony for too many years.
+Try something different.
+Surrender.
+	`)
+}
