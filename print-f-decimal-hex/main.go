5d1d247498a675a4c73ccb3f5c6074eeca88ecc9
M Rizki Ramadhan <p79-muhammad.ramadan@edot-dev.com>
2026-05-24 16:36:31 +0700

    print-f-decimal-hex

---
+++ print-f-decimal-hex/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b, c, d, e, f := 0, 1, 2, 3, 4, 5
+
+	fmt.Printf("%v \t %b \t %#X\n", a, a, a)
+	fmt.Printf("%v \t %b \t %#X\n", b, b, b)
+	fmt.Printf("%v \t %b \t %#X\n", c, c, c)
+	fmt.Printf("%v \t %b \t %#X\n", d, d, d)
+	fmt.Printf("%v \t %b \t %#X\n", e, e, e)
+	fmt.Printf("%v \t %b \t %#X\n", f, f, f)	
+}
